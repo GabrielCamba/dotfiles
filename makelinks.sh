2f5efcdff30db7eab5179159c657ad9623ca1607
Gabriel Camba <gabriel@camba.coop>
2020-09-29 22:22:43 -0300

    added ohmyzsh, vundle and molokai to install script

--- makelinks.sh
+++ makelinks.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh install.sh
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+cp -r ./colors ~/.vim
+
 locations="tmux.conf zshrc oh-my-zsh vimrc"  
 
 for location in $locations; do
     echo "Creating symlink to $location into my home folder"
     ln -s dotfiles/$location/.$location ~/.$location
 done
-
